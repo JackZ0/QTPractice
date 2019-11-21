9d426eeed11a7959f72b3ba5e14b6af29d3f7a02
Zhang Hao <2162990048@qq.com>
2019-11-21 09:28:26 +0800

    add log4qttestt 用于qtlog日志打印信息使用的

---
+++ log4qt-master/tests/debug/moc_log4qttest.cpp
@@ -0,0 +1,406 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'log4qttest.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../log4qttest.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'log4qttest.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Log4QtTest_t {
+    QByteArrayData data[65];
+    char stringdata0[1552];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Log4QtTest_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Log4QtTest_t qt_meta_stringdata_Log4QtTest = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "Log4QtTest"
+QT_MOC_LITERAL(1, 11, 12), // "initTestCase"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 15), // "cleanupTestCase"
+QT_MOC_LITERAL(4, 41, 25), // "DateTime_compability_data"
+QT_MOC_LITERAL(5, 67, 20), // "DateTime_compability"
+QT_MOC_LITERAL(6, 88, 18), // "DateTime_week_data"
+QT_MOC_LITERAL(7, 107, 13), // "DateTime_week"
+QT_MOC_LITERAL(8, 121, 26), // "DateTime_milliseconds_data"
+QT_MOC_LITERAL(9, 148, 21), // "DateTime_milliseconds"
+QT_MOC_LITERAL(10, 170, 21), // "PatternFormatter_data"
+QT_MOC_LITERAL(11, 192, 16), // "PatternFormatter"
+QT_MOC_LITERAL(12, 209, 23), // "Properties_default_data"
+QT_MOC_LITERAL(13, 233, 18), // "Properties_default"
+QT_MOC_LITERAL(14, 252, 16), // "Properties_names"
+QT_MOC_LITERAL(15, 269, 27), // "Properties_load_device_data"
+QT_MOC_LITERAL(16, 297, 22), // "Properties_load_device"
+QT_MOC_LITERAL(17, 320, 24), // "Properties_load_settings"
+QT_MOC_LITERAL(18, 345, 28), // "OptionConverter_boolean_data"
+QT_MOC_LITERAL(19, 374, 23), // "OptionConverter_boolean"
+QT_MOC_LITERAL(20, 398, 29), // "OptionConverter_filesize_data"
+QT_MOC_LITERAL(21, 428, 24), // "OptionConverter_filesize"
+QT_MOC_LITERAL(22, 453, 24), // "OptionConverter_int_data"
+QT_MOC_LITERAL(23, 478, 19), // "OptionConverter_int"
+QT_MOC_LITERAL(24, 498, 26), // "OptionConverter_level_data"
+QT_MOC_LITERAL(25, 525, 21), // "OptionConverter_level"
+QT_MOC_LITERAL(26, 547, 33), // "OptionConverter_substitution_..."
+QT_MOC_LITERAL(27, 581, 28), // "OptionConverter_substitution"
+QT_MOC_LITERAL(28, 610, 27), // "OptionConverter_target_data"
+QT_MOC_LITERAL(29, 638, 22), // "OptionConverter_target"
+QT_MOC_LITERAL(30, 661, 27), // "Factory_createAppender_data"
+QT_MOC_LITERAL(31, 689, 22), // "Factory_createAppender"
+QT_MOC_LITERAL(32, 712, 25), // "Factory_createFilter_data"
+QT_MOC_LITERAL(33, 738, 20), // "Factory_createFilter"
+QT_MOC_LITERAL(34, 759, 25), // "Factory_createLayout_data"
+QT_MOC_LITERAL(35, 785, 20), // "Factory_createLayout"
+QT_MOC_LITERAL(36, 806, 30), // "Factory_setObjectProperty_data"
+QT_MOC_LITERAL(37, 837, 25), // "Factory_setObjectProperty"
+QT_MOC_LITERAL(38, 863, 12), // "ListAppender"
+QT_MOC_LITERAL(39, 876, 13), // "DenyAllFilter"
+QT_MOC_LITERAL(40, 890, 21), // "LevelMatchFilter_data"
+QT_MOC_LITERAL(41, 912, 16), // "LevelMatchFilter"
+QT_MOC_LITERAL(42, 929, 21), // "LevelRangeFilter_data"
+QT_MOC_LITERAL(43, 951, 16), // "LevelRangeFilter"
+QT_MOC_LITERAL(44, 968, 22), // "StringMatchFilter_data"
+QT_MOC_LITERAL(45, 991, 17), // "StringMatchFilter"
+QT_MOC_LITERAL(46, 1009, 26), // "AppenderSkeleton_threshold"
+QT_MOC_LITERAL(47, 1036, 28), // "AppenderSkeleton_filter_data"
+QT_MOC_LITERAL(48, 1065, 23), // "AppenderSkeleton_filter"
+QT_MOC_LITERAL(49, 1089, 17), // "BasicConfigurator"
+QT_MOC_LITERAL(50, 1107, 12), // "FileAppender"
+QT_MOC_LITERAL(51, 1120, 24), // "DailyRollingFileAppender"
+QT_MOC_LITERAL(52, 1145, 24), // "LoggingEvent_stream_data"
+QT_MOC_LITERAL(53, 1170, 19), // "LoggingEvent_stream"
+QT_MOC_LITERAL(54, 1190, 29), // "LogManager_configureLogLogger"
+QT_MOC_LITERAL(55, 1220, 37), // "PropertyConfigurator_missing_..."
+QT_MOC_LITERAL(56, 1258, 43), // "PropertyConfigurator_unknown_..."
+QT_MOC_LITERAL(57, 1302, 35), // "PropertyConfigurator_missing_..."
+QT_MOC_LITERAL(58, 1338, 41), // "PropertyConfigurator_unknown_..."
+QT_MOC_LITERAL(59, 1380, 26), // "PropertyConfigurator_reset"
+QT_MOC_LITERAL(60, 1407, 26), // "PropertyConfigurator_debug"
+QT_MOC_LITERAL(61, 1434, 30), // "PropertyConfigurator_threshold"
+QT_MOC_LITERAL(62, 1465, 37), // "PropertyConfigurator_handleQt..."
+QT_MOC_LITERAL(63, 1503, 28), // "PropertyConfigurator_example"
+QT_MOC_LITERAL(64, 1532, 19) // "RollingFileAppender"
+
+    },
+    "Log4QtTest\0initTestCase\0\0cleanupTestCase\0"
+    "DateTime_compability_data\0"
+    "DateTime_compability\0DateTime_week_data\0"
+    "DateTime_week\0DateTime_milliseconds_data\0"
+    "DateTime_milliseconds\0PatternFormatter_data\0"
+    "PatternFormatter\0Properties_default_data\0"
+    "Properties_default\0Properties_names\0"
+    "Properties_load_device_data\0"
+    "Properties_load_device\0Properties_load_settings\0"
+    "OptionConverter_boolean_data\0"
+    "OptionConverter_boolean\0"
+    "OptionConverter_filesize_data\0"
+    "OptionConverter_filesize\0"
+    "OptionConverter_int_data\0OptionConverter_int\0"
+    "OptionConverter_level_data\0"
+    "OptionConverter_level\0"
+    "OptionConverter_substitution_data\0"
+    "OptionConverter_substitution\0"
+    "OptionConverter_target_data\0"
+    "OptionConverter_target\0"
+    "Factory_createAppender_data\0"
+    "Factory_createAppender\0Factory_createFilter_data\0"
+    "Factory_createFilter\0Factory_createLayout_data\0"
+    "Factory_createLayout\0"
+    "Factory_setObjectProperty_data\0"
+    "Factory_setObjectProperty\0ListAppender\0"
+    "DenyAllFilter\0LevelMatchFilter_data\0"
+    "LevelMatchFilter\0LevelRangeFilter_data\0"
+    "LevelRangeFilter\0StringMatchFilter_data\0"
+    "StringMatchFilter\0AppenderSkeleton_threshold\0"
+    "AppenderSkeleton_filter_data\0"
+    "AppenderSkeleton_filter\0BasicConfigurator\0"
+    "FileAppender\0DailyRollingFileAppender\0"
+    "LoggingEvent_stream_data\0LoggingEvent_stream\0"
+    "LogManager_configureLogLogger\0"
+    "PropertyConfigurator_missing_appender\0"
+    "PropertyConfigurator_unknown_appender_class\0"
+    "PropertyConfigurator_missing_layout\0"
+    "PropertyConfigurator_unknown_layout_class\0"
+    "PropertyConfigurator_reset\0"
+    "PropertyConfigurator_debug\0"
+    "PropertyConfigurator_threshold\0"
+    "PropertyConfigurator_handleQtMessages\0"
+    "PropertyConfigurator_example\0"
+    "RollingFileAppender"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Log4QtTest[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      63,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  329,    2, 0x08 /* Private */,
+       3,    0,  330,    2, 0x08 /* Private */,
+       4,    0,  331,    2, 0x08 /* Private */,
+       5,    0,  332,    2, 0x08 /* Private */,
+       6,    0,  333,    2, 0x08 /* Private */,
+       7,    0,  334,    2, 0x08 /* Private */,
+       8,    0,  335,    2, 0x08 /* Private */,
+       9,    0,  336,    2, 0x08 /* Private */,
+      10,    0,  337,    2, 0x08 /* Private */,
+      11,    0,  338,    2, 0x08 /* Private */,
+      12,    0,  339,    2, 0x08 /* Private */,
+      13,    0,  340,    2, 0x08 /* Private */,
+      14,    0,  341,    2, 0x08 /* Private */,
+      15,    0,  342,    2, 0x08 /* Private */,
+      16,    0,  343,    2, 0x08 /* Private */,
+      17,    0,  344,    2, 0x08 /* Private */,
+      18,    0,  345,    2, 0x08 /* Private */,
+      19,    0,  346,    2, 0x08 /* Private */,
+      20,    0,  347,    2, 0x08 /* Private */,
+      21,    0,  348,    2, 0x08 /* Private */,
+      22,    0,  349,    2, 0x08 /* Private */,
+      23,    0,  350,    2, 0x08 /* Private */,
+      24,    0,  351,    2, 0x08 /* Private */,
+      25,    0,  352,    2, 0x08 /* Private */,
+      26,    0,  353,    2, 0x08 /* Private */,
+      27,    0,  354,    2, 0x08 /* Private */,
+      28,    0,  355,    2, 0x08 /* Private */,
+      29,    0,  356,    2, 0x08 /* Private */,
+      30,    0,  357,    2, 0x08 /* Private */,
+      31,    0,  358,    2, 0x08 /* Private */,
+      32,    0,  359,    2, 0x08 /* Private */,
+      33,    0,  360,    2, 0x08 /* Private */,
+      34,    0,  361,    2, 0x08 /* Private */,
+      35,    0,  362,    2, 0x08 /* Private */,
+      36,    0,  363,    2, 0x08 /* Private */,
+      37,    0,  364,    2, 0x08 /* Private */,
+      38,    0,  365,    2, 0x08 /* Private */,
+      39,    0,  366,    2, 0x08 /* Private */,
+      40,    0,  367,    2, 0x08 /* Private */,
+      41,    0,  368,    2, 0x08 /* Private */,
+      42,    0,  369,    2, 0x08 /* Private */,
+      43,    0,  370,    2, 0x08 /* Private */,
+      44,    0,  371,    2, 0x08 /* Private */,
+      45,    0,  372,    2, 0x08 /* Private */,
+      46,    0,  373,    2, 0x08 /* Private */,
+      47,    0,  374,    2, 0x08 /* Private */,
+      48,    0,  375,    2, 0x08 /* Private */,
+      49,    0,  376,    2, 0x08 /* Private */,
+      50,    0,  377,    2, 0x08 /* Private */,
+      51,    0,  378,    2, 0x08 /* Private */,
+      52,    0,  379,    2, 0x08 /* Private */,
+      53,    0,  380,    2, 0x08 /* Private */,
+      54,    0,  381,    2, 0x08 /* Private */,
+      55,    0,  382,    2, 0x08 /* Private */,
+      56,    0,  383,    2, 0x08 /* Private */,
+      57,    0,  384,    2, 0x08 /* Private */,
+      58,    0,  385,    2, 0x08 /* Private */,
+      59,    0,  386,    2, 0x08 /* Private */,
+      60,    0,  387,    2, 0x08 /* Private */,
+      61,    0,  388,    2, 0x08 /* Private */,
+      62,    0,  389,    2, 0x08 /* Private */,
+      63,    0,  390,    2, 0x08 /* Private */,
+      64,    0,  391,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Log4QtTest::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Log4QtTest *_t = static_cast<Log4QtTest *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->initTestCase(); break;
+        case 1: _t->cleanupTestCase(); break;
+        case 2: _t->DateTime_compability_data(); break;
+        case 3: _t->DateTime_compability(); break;
+        case 4: _t->DateTime_week_data(); break;
+        case 5: _t->DateTime_week(); break;
+        case 6: _t->DateTime_milliseconds_data(); break;
+        case 7: _t->DateTime_milliseconds(); break;
+        case 8: _t->PatternFormatter_data(); break;
+        case 9: _t->PatternFormatter(); break;
+        case 10: _t->Properties_default_data(); break;
+        case 11: _t->Properties_default(); break;
+        case 12: _t->Properties_names(); break;
+        case 13: _t->Properties_load_device_data(); break;
+        case 14: _t->Properties_load_device(); break;
+        case 15: _t->Properties_load_settings(); break;
+        case 16: _t->OptionConverter_boolean_data(); break;
+        case 17: _t->OptionConverter_boolean(); break;
+        case 18: _t->OptionConverter_filesize_data(); break;
+        case 19: _t->OptionConverter_filesize(); break;
+        case 20: _t->OptionConverter_int_data(); break;
+        case 21: _t->OptionConverter_int(); break;
+        case 22: _t->OptionConverter_level_data(); break;
+        case 23: _t->OptionConverter_level(); break;
+        case 24: _t->OptionConverter_substitution_data(); break;
+        case 25: _t->OptionConverter_substitution(); break;
+        case 26: _t->OptionConverter_target_data(); break;
+        case 27: _t->OptionConverter_target(); break;
+        case 28: _t->Factory_createAppender_data(); break;
+        case 29: _t->Factory_createAppender(); break;
+        case 30: _t->Factory_createFilter_data(); break;
+        case 31: _t->Factory_createFilter(); break;
+        case 32: _t->Factory_createLayout_data(); break;
+        case 33: _t->Factory_createLayout(); break;
+        case 34: _t->Factory_setObjectProperty_data(); break;
+        case 35: _t->Factory_setObjectProperty(); break;
+        case 36: _t->ListAppender(); break;
+        case 37: _t->DenyAllFilter(); break;
+        case 38: _t->LevelMatchFilter_data(); break;
+        case 39: _t->LevelMatchFilter(); break;
+        case 40: _t->LevelRangeFilter_data(); break;
+        case 41: _t->LevelRangeFilter(); break;
+        case 42: _t->StringMatchFilter_data(); break;
+        case 43: _t->StringMatchFilter(); break;
+        case 44: _t->AppenderSkeleton_threshold(); break;
+        case 45: _t->AppenderSkeleton_filter_data(); break;
+        case 46: _t->AppenderSkeleton_filter(); break;
+        case 47: _t->BasicConfigurator(); break;
+        case 48: _t->FileAppender(); break;
+        case 49: _t->DailyRollingFileAppender(); break;
+        case 50: _t->LoggingEvent_stream_data(); break;
+        case 51: _t->LoggingEvent_stream(); break;
+        case 52: _t->LogManager_configureLogLogger(); break;
+        case 53: _t->PropertyConfigurator_missing_appender(); break;
+        case 54: _t->PropertyConfigurator_unknown_appender_class(); break;
+        case 55: _t->PropertyConfigurator_missing_layout(); break;
+        case 56: _t->PropertyConfigurator_unknown_layout_class(); break;
+        case 57: _t->PropertyConfigurator_reset(); break;
+        case 58: _t->PropertyConfigurator_debug(); break;
+        case 59: _t->PropertyConfigurator_threshold(); break;
+        case 60: _t->PropertyConfigurator_handleQtMessages(); break;
+        case 61: _t->PropertyConfigurator_example(); break;
+        case 62: _t->RollingFileAppender(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject Log4QtTest::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_Log4QtTest.data,
+      qt_meta_data_Log4QtTest,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *Log4QtTest::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Log4QtTest::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Log4QtTest.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Log4QtTest::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 63)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 63;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 63)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 63;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
