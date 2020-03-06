a76607a3dd71bb2ff8af37be3a7e133c581d47a7
Zhang Hao <2162990048@qq.com>
2020-03-06 10:40:16 +0800

    增加画直线

---
+++ Chess/Chess/MainWindow.cpp
@@ -0,0 +1,22 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    //绘制类
+    // 绘制打印机、绘图、考屏幕
+    QPainter painter(this);
+    painter.drawLine(QPoint(0,0),QPoint(100,100));
+}
