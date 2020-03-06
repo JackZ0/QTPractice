b9295d6a7e95f25f776a7919a748c2381b5c2983
Zhang Hao <2162990048@qq.com>
2020-03-06 10:44:09 +0800

    画圆

--- Chess/Chess/MainWindow.cpp
+++ Chess/Chess/MainWindow.cpp
@@ -18,5 +18,14 @@ void MainWindow::paintEvent(QPaintEvent *)
     //绘制类
     // 绘制打印机、绘图、考屏幕
     QPainter painter(this);
+
+    painter.setPen(QColor(255,0,255));
+    painter.setBrush(QBrush(QColor(255,255,0)));
+
+
     painter.drawLine(QPoint(0,0),QPoint(100,100));
+
+
+    painter.drawText(200,200,"hello chess!");
+    painter.drawEllipse(QPoint(200,300),50,50);
 }
