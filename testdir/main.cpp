638b07dab287775444f7e1179e1fee9a805678e3
Zhang Hao <2162990048@qq.com>
2020-04-21 18:38:44 +0800

    添加新文件

---
+++ testdir/main.cpp
@@ -0,0 +1,98 @@
+#include "MainWindow.h"
+#include <QApplication>
+#include <QDir>
+#include <QTime>
+
+#include <QDebug>
+QStringList filelist;
+int k;
+int FindFile(const QString& _filePath)
+{
+
+    QDir dir(_filePath);
+    if (!dir.exists()) {
+        return -1;
+    }
+  //取到所有的文件和文件名，但是去掉.和..的文件夹（这是QT默认有的）
+    dir.setFilter(QDir::Dirs|QDir::Files|QDir::NoDotAndDotDot);
+    //文件夹优先
+    dir.setSorting(QDir::DirsFirst);
+    //转化成一个list
+    QFileInfoList list = dir.entryInfoList();
+    QStringList infolist = dir.entryList(QDir::Files | QDir::NoDotAndDotDot);
+    if(list.size()< 1 ) {
+        return -1;
+    }
+    int i=0;
+    //递归算法的核心部分
+    do{
+        QFileInfo fileInfo = list.at(i);
+        //如果是文件夹，递归
+        bool bisDir = fileInfo.isDir();
+        if(bisDir) {
+            FindFile(fileInfo.filePath());
+        }
+        else{
+            for(int m = 0; m <infolist.size(); m++) {
+                                //这里是获取当前要处理的文件名
+                qDebug() << infolist.at(m);
+                QString src=fileInfo.filePath();
+                filelist.append(src);
+                 qDebug() << "src" << src;
+                QString dst = "D://111111/"+QString::number(k)+".jpg";
+                QFile file(src);
+                file.copy(dst);
+
+                k++;
+            }
+            break;
+        }//end else
+        i++;
+
+//        qDebug() <<fileInfo.filePath() << "||"<< src << endl;
+
+    } while(i < list.size());
+}
+int main(int argc, char *argv[])
+{
+    int j = 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    QString _filePath = "D://111111";
+    QDir dir(_filePath);
+        if (!dir.exists()) {
+            return -1;
+        }
+
+      //取到所有的文件和文件名，但是去掉.和..的文件夹（这是QT默认有的）
+        dir.setFilter(QDir::Dirs|QDir::Files|QDir::NoDotAndDotDot);
+
+        //文件夹优先
+        dir.setSorting(QDir::DirsFirst);
+
+        //转化成一个list
+        QFileInfoList list = dir.entryInfoList();
+        if(list.size()< 1 ) {
+            return -1;
+        }
+        int i=0;
+
+        //递归算法的核心部分
+        do{
+            QFileInfo fileInfo = list.at(i);
+            //如果是文件夹，递归
+            bool bisDir = fileInfo.isDir();
+            if(bisDir) {
+                FindFile(fileInfo.filePath());
+            }
+            else{
+                //bool isDll = fileInfo.fileName().endsWith(".dll");
+//                qDebug() << fileInfo.filePath() << ":" <<fileInfo.fileName();
+
+            }//end else
+            i++;
+        } while(i < list.size());
+
+    return a.exec();
+}
