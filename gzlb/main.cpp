2f7d6748416dbb7764c0a0c8b6657919a5c7f10c
jackZ0 <2162990048@qq.com>
2022-02-22 17:00:06 +0800

    修改皮肤

--- gzlb/main.cpp
+++ gzlb/main.cpp
@@ -3,20 +3,29 @@
 #include <QApplication>
 #include <QFile>
 #include <QDebug>
-int main(int argc, char *argv[])
+
+void initSkin();
+void initSkin()
 {
-    QApplication a(argc, argv);
-    QFile qss(":/res/1.qss");
-    if(qss.open(QFile::ReadOnly))
+    QFile file(QStringLiteral(":/res/1.qss") );
+    if(file.open(QFile::ReadOnly))
     {
-        qDebug() << "open success!" << endl;
-        QString style = QLatin1String(qss.readAll());
-        a.setStyleSheet(style);
+         qDebug() << "open success!" << endl;
+         QTextStream filetext(&file);
+         QString stylesheet = filetext.readAll();
+         qApp->setStyleSheet(stylesheet);
     }
     else
     {
         qDebug() << "open failed" << endl;
     }
+
+    file.close();
+}
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    initSkin();
     MainWindow w;
     w.show();
     return a.exec();
